e5d29bf07b1c2492bc196c8d73a73611c2e1c921
Zach Meadows <zmeadows1@gmail.com>
2019-10-17 16:07:30 -0400

    projects are flipcards now

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,22 +2,40 @@ import React from "react";
 
 function Example(props) {
   return (
-    <a href={props.url}>
-      <div className="example">
-        <h3>{props.name}</h3>
+    <div className="flipContain">
+      <div className="flipInner">
+        <div className="exampleFront">
+          <h3>{props.name}</h3>
 
-        <div
-          className="exImg"
-          style={{ backgroundImage: `url(${props.image})` }}
-        />
-        <div className="uselist">
-          {props.used.map((item, i) => {
-            return <p key={i}>{item}</p>;
-          })}
+          <div
+            className="exImg"
+            style={{ backgroundImage: `url(${props.image})` }}
+          />
+          <div className="uselist">
+            {props.used.map((item, i) => {
+              return <p key={i}>{item}</p>;
+            })}
+          </div>
         </div>
-        <p>{props.info}</p>
+        <div className="exampleBack">
+          <h3>{props.name}</h3>
+          <p>{props.info}</p>
+          <a href={props.url} className="exampleLink">Check out this Project</a>
+        </div>
       </div>
-    </a>
+    </div>
+    //     <div className="flip-box">
+
+    //   <div className="flip-box-inner">
+    //     <div className="flip-box-front">
+    //       <h2>Front Side</h2>
+    //     </div>
+    //     <div className="flip-box-back">
+    //       <h2>Back Side</h2>
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 }
 
