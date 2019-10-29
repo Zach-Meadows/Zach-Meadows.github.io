33fd99baa2802d63a4dd9519bf5ffe567e47af59
Zach Meadows <zmeadows1@gmail.com>
2019-10-29 17:01:31 -0400

    adding art examples

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import ArtEx from './ArtEx'
 import MC from "../images/halo.jpg"
-
+import lax from "../images/Snorlax.jpg"
+import big from "../images/Big.jpg"
 const manyArts = [
     {
+        name: "Snorlax",
+        info: "Full size realistic Snorlax from Pokemon.",
+        image: lax,
+    },
+    {
+        name: "Big the Cat",
+        info: "Full size realistic Big the Cat from Sonic the Hedgehog series.",
+        image: big,
+    },
+    {
         name: "Master Cheif",
-        url: "/",
         info: "Master Cheif armor from Halo.",
         image: MC,
-        used: []
-    }
+    },
+  
 ]
 
 function Art(){
     return(
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column"
+        }}>
             {manyArts.map((item, i) => {
                 return <ArtEx {...item} key={i}/>
             })}
--- src/components/ArtEx.js
+++ src/components/ArtEx.js
@@ -3,17 +3,25 @@ import React from "react"
 function ArtEx(props){
     return(
         <div style={{
-            display: 'flex'
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            width: "90vw",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            margin: "10px",
+            borderRadius: "20px",
+            fontFamily: "'Comfortaa', cursive"
         }}>
-        <div style={{
-            backgroundImage: `url(${props.image})`,
-            backgroundSize: 'contain',
-            backgroundRepeat: "no-repeat",
-            width: "500px",
-            height: "500px"
-        }}>
-
-        </div>
+                <h2>{props.name}</h2>
+                <a href="https://www.facebook.com/BlackMeadowStudio/" style={{
+                    
+                }}>More</a>
+                <p>{props.info}</p>
+        <img src={props.image} style={{
+            borderRadius: "0 0 20px 20px",
+            width: "90vw"
+        }}/>
+       
         </div>
     )
 }
