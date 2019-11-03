194a36190188393a33012bf1a0a34b5923bbd8c4
Zach Meadows <zmeadows1@gmail.com>
2019-11-03 12:56:44 -0500

    contact form works

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,64 +1,64 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const FormContainer = styled.div`
-background-color: rgba(255, 255, 255, 0.8);
-margin: 5px;
-padding: 1px 5px;
-border-radius: 10px;
-font-family: 'Comfortaa', cursive;
-width: 80vw;
-`
+  background-color: rgba(255, 255, 255, 0.8);
+  margin: 5px;
+  padding: 1px 5px;
+  border-radius: 10px;
+  font-family: "Comfortaa", cursive;
+  width: 80vw;
+`;
 const StyledText = styled.textarea`
-resize: vertical;
-min-height: 200px;
-max-height: 400px;
-width: 80%;
-overflow: scroll;
-margin: 5px;
-font-size: 15px;
-`
+  resize: vertical;
+  min-height: 200px;
+  max-height: 400px;
+  width: 80%;
+  overflow: scroll;
+  margin: 5px;
+  font-size: 15px;
+`;
 const StyledInput = styled.input`
-margin: 5px;
-font-size: 15px;
-
+  margin: 5px;
+  font-size: 15px;
+`;
+const EmailForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
+`;
+const FormLabel = styled.label`
+margin-top: 10px;
 `
 
 function Contact() {
   return (
     <FormContainer>
-      <form onSubmit={e => this.formSubmit(e)} style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: 'center',
-          flexDirection: "column",
-          margin: '10px'
-      }}>
-        
-
-        <label htmlFor="message-name">Your Name</label>
-        <StyledInput name="name" type="text" placeholder="Your Name" />
-
-        <label htmlFor="message-email">Your Email</label>
+      <EmailForm
+        action="https://formsubmit.co/0f172a1551c7a4961f7b5d591d8145e6"
+        method="POST"
+        target="_blank"
+      >
+      <FormLabel>Your Name</FormLabel>
+        <StyledInput type="text" name="name" placeholder="Name"/>
+      <FormLabel>Your Email</FormLabel>
         <StyledInput
-          name="email"
           type="email"
+          name="email"
           placeholder="your@email.com"
           required
         />
-
-  
-        <label htmlFor="message-input">Your Message</label>
+        <FormLabel>Your Message</FormLabel>
         <StyledText
           name="message"
-          type="text"
-          placeholder="Please write your message here"
+          placeholder="Please write your message here."
           required
         />
-              <div>
-          <button type="submit">Send Email</button>
-        </div>
-      </form>
+        <button type="submit">Send Email</button>
+        <FormLabel style={{fontSize: "12px"}}>(This will open a new window.)</FormLabel>
+      </EmailForm>
     </FormContainer>
   );
 }
