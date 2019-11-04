2c37254d0a33e1d2b92e7b5109028c51ff31992b
Zach Meadows <zmeadows1@gmail.com>
2019-11-03 21:16:19 -0500

    added skill logos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,52 @@
 import React from "react"
-const lotus = require('lotus-ipsum')
+import styled from "styled-components"
+import css from '../images/css.png'
+import html from '../images/html.png'
+import mongo from '../images/mongo.png'
+import python from '../images/python.png'
+import ReactIcon from '../images/react.png'
+import ReduxIcon from '../images/Redux.png'
+import JS from '../images/javascript.svg'
+
+const ResumeContain = styled.div`
+width: 90vw;
+background-color: rgba(255, 255, 255, 0.8);
+margin: 10px;
+padding: 1px 5px;
+border-radius: 10px;
+font-family: 'Comfortaa', cursive; 
+`
+const FlexSkill = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-evenly;
+align-items: center;
+margin: 10px
+`
+
+const SkillImage = styled.img`
+width: 20%;
+height: 100px;
+margin: 10px;
+object-fit: contain;
+position: center;
+`
+const ArraySkills = [html,css,JS,mongo,python,ReactIcon,ReduxIcon]
 
 function Home() {
     return( 
-        <div className="text" style={{
-            width: "90vw"
-        }}>
+        <ResumeContain>
             <h2>Resume</h2>
             <div>
                 <h3>Skills</h3>
-
+                <FlexSkill>
+                {ArraySkills.map((item, i)=>{
+                    return (
+                        <SkillImage src={item} key={i}></SkillImage>
+                    )
+                })}</FlexSkill>
             </div>
-        </div>
+        </ResumeContain>
     )
 }
 
